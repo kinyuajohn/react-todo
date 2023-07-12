0fc13d3e6ecf2c139c86053c84f3d2ec38638e1a
John Kinya <kingjohnkn@gmail.com>
2023-07-12 15:06:44 +0300

    TodoList

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  return (
+    <div>
+      <h1>What's the plan for today?</h1>
+      <TodoForm />
+    </div>
+  );
+}
+
+export default TodoList;
