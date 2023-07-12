cdcb4ba776dbccc732bc2a4d57070d28271fe4f0
John Kinya <kingjohnkn@gmail.com>
2023-07-12 15:24:35 +0300

    modify todoform, todolist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,10 +3,21 @@ import TodoForm from "./TodoForm";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s+$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+  };
+
   return (
     <div>
       <h1>What's the plan for today?</h1>
-      <TodoForm />
+      <TodoForm onSubmit={addTodo} />
     </div>
   );
 }
